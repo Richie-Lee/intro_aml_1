e332f58dd149507f2ab862b1d3a88adbb396a84d
Richie Lee <t-richielee@microsoft.com>
2024-05-08 17:37:31 +0200

    added placeholder automl

--- src/model/train.py
+++ src/model/train.py
@@ -13,6 +13,8 @@ from sklearn.linear_model import LogisticRegression
 
 # define functions
 def main(args):
+    # TO DO: enable autologging
+
     # read data
     df = get_csvs_df(args.training_data)
 
